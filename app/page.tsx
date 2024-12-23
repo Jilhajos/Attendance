4a06e4fc924148fde57d14dcb7cd0c64b10500ee
Jilha jos <93869128+Jilhajos@users.noreply.github.com>
2024-12-23 11:06:14 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,11 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-
 const Home = () => {
   const router = useRouter();
-
   useEffect(() => {
-    // Redirect to the login page after the page is mounted
     router.push('/login');
-  }, [router]); // Only run once when the component mounts
-
-  return <div>Loading...</div>; // Optionally display loading state
+  }, [router]); 
+  return <div>Loading...</div>; 
 };
-
 export default Home;
